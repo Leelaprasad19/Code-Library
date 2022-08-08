68f74273a9688aa486c4f4987e8b56501e83963b
Leelaprasad19 <59998911+Leelaprasad19@users.noreply.github.com>
2022-08-08 22:05:36 +0530

    Length of string using recursion

--- recurssion length of string.c
+++ recurssion length of string.c
@@ -1,19 +1,24 @@
-#include<stdio.h>
-#include<stdlib.h>
-int LenStr(char* str)
-{
-    if (*str == '\0')
-        return 0;
-    else
-        return 1 + LenStr(str + 1);
+#include <stdio.h>
+
+int stringLength(char s[]){
+    
+    int i = 0;
+    
+    while(s[i]!='\0'){
+        i+=1;
+    }
+    
+    return i;
 }
 
-int main()
-{
-    int len;
-    char str[20];
-    scanf("%s",str);
-    len= LenStr(str);
-    printf("Length of the string = %d",len);
-    return 0;
+int main() {
+	
+	char s[100];
+	
+	scanf("%s", s);
+	
+	printf("%d",stringLength(s));
+	
+	return 0;
 }
+
