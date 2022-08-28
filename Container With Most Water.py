bfe734474f19f0069aad661878d6c973194191e3
Leelaprasad19 <59998911+Leelaprasad19@users.noreply.github.com>
2022-08-28 15:33:42 +0530

    Find maximum area between two vertical lines

---
+++ Container With Most Water.py
@@ -0,0 +1,20 @@
+def maxArea(self, height: List[int]) -> int:
+        
+        n = len(height)
+        
+        i = 0
+        j = n-1
+        sol = 0
+        while(i < j):
+            if(height[i] < height[j]):
+                a = height[i] * (j - i)
+                i = i + 1
+            else:
+                a = height[j] * (j - i)
+                j = j - 1
+                
+            if(a > sol):
+                sol = a
+        
+        return sol
+        
